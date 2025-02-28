f9f26890a17813b4122c27224a8a70b5c979b58e
fahim <fahim1998108@gmail.com>
2025-03-01 05:12:11 +0530

    Update

--- src/Model/AuthData.tsx
+++ src/Model/AuthData.tsx
@@ -2,12 +2,12 @@ export class AuthData {
     id: string;
     username: string;
     email: string;
-    password: string;  // ✅ Added password field
+    password: string;  
   
     constructor(id: string, username: string, email: string, password: string) {
       this.id = id;
       this.username = username;
       this.email = email;
-      this.password = password; // ✅ Store password
+      this.password = password; 
     }
 }
